58db4feb8ad0988f4d396671b177f78519870bbf
8sxwmrswgv-spec <8sxwmrswgv@privaterelay.appleid.com>
2026-05-13 17:56:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -183,6 +183,7 @@ app.post("/create-checkout-session", async (req, res) => {
           quantity: 1
         }
       ],
+      allow_promotion_codes: true,
       success_url: `${process.env.FRONTEND_URL}/?payment=success&session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.FRONTEND_URL}/?payment=cancelled`,
       metadata: { email }
